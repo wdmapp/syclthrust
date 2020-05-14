047bedc1e038c47dc6b7823b7e897799159bd306
Bryce Allen <bdallen@uchicago.edu>
2020-05-14 15:50:12 -0400

    update host vector

--- include/thrust/host_vector.h
+++ include/thrust/host_vector.h
@@ -19,16 +19,19 @@ namespace thrust {
  */
 template <typename T>
 class host_vector {
-  using value_type = T;
-  using size_type = typename std::size_t;
-  using host_alloc_type = cl::sycl::usm_allocator<T, cl::sycl::usm::alloc::host>;
-  using host_vector_type = std::vector<T, host_alloc_type>;
-
-  private:
-    cl::sycl::queue& m_queue;
-    host_vector_type m_vec;
-
   public:
+    using value_type = T;
+
+    using host_alloc_type = cl::sycl::usm_allocator<value_type,
+                                                cl::sycl::usm::alloc::host>;
+    using host_vector_type = std::vector<value_type, host_alloc_type>;
+
+    using pointer = typename host_vector_type::pointer;
+    using const_pointer = typename host_vector_type::const_pointer;
+    using reference = typename host_vector_type::reference;
+    using const_reference = typename host_vector_type::const_reference;
+    using size_type = typename host_vector_type::size_type;
+
     host_vector(size_type count) : m_queue(thrust::sycl::get_queue()),
                                      m_vec(count, host_alloc_type(m_queue)) {}
     host_vector() : host_vector(0) {}
@@ -37,18 +40,17 @@ class host_vector {
     host_vector(const host_vector &dv)
       : m_queue(thrust::sycl::get_queue()),
         m_vec(dv.m_vec.size(), host_alloc_type(m_queue)) {
-      m_queue.memcpy(m_vec.data(), dv.data(),
-                     min(m_vec.size(),dv.size())*sizeof(T));
+      assert(m_vec.size() == dv.m_vec.size());
+      m_queue.memcpy(m_vec.data(), dv.data(), m_vec.size()*sizeof(T));
       m_queue.wait();
     }
-    host_vector(host_vector &&dv)
-      : m_queue(std::move(dv.m_queue)),
-        m_vec(std::move(dv.m_vec)) {}
+    host_vector(host_vector &&dv) = delete;
 
-    // operators
-    T& operator[](size_type i);
-    const T& operator[](size_type i) const;
+    // element access
+    reference operator[](size_type i);
+    const_reference operator[](size_type i) const;
 
+    // assignment
     host_vector& operator=(const host_vector &dv) {
       resize(dv.size());
       m_queue.memcpy(data(), dv.data(), size()*sizeof(T));
@@ -57,20 +59,27 @@ class host_vector {
       return *this;
     }
 
+    // Note: trying to move the vec results in a complex error related to
+    // the sycl usm_allocator
+    host_vector& operator=(host_vector &&dv) = delete;
+    /*
     host_vector& operator=(host_vector &&dv) {
         m_vec = std::move(dv.m_vec);
-
-        // TODO: deinit vector
-        //v.m_vec = v.host_vector_type(0, v.host_alloc_type(v.m_queue));
+        m_queue = std::move(dv.m_queue);
 
         return *this;
     }
+    */
 
     // functions
     void resize(size_type new_size);
     size_type size() const;
-    T* data();
-    const T* data() const;
+    pointer data();
+    const_pointer data() const;
+
+  private:
+    cl::sycl::queue& m_queue;
+    host_vector_type m_vec;
 };
 
 
@@ -82,30 +91,35 @@ inline void host_vector<T>::resize(host_vector::size_type new_size) {
 
 
 template <typename T>
-inline T& host_vector<T>::operator[](host_vector::size_type i) {
+inline typename host_vector<T>::reference
+host_vector<T>::operator[](host_vector::size_type i) {
   return m_vec[i];
 }
 
 
 template <typename T>
-inline const T& host_vector<T>::operator[](host_vector::size_type i) const {
+inline typename host_vector<T>::const_reference
+host_vector<T>::operator[](host_vector::size_type i) const {
   return m_vec[i];
 }
 
 
 template <typename T>
-inline typename host_vector<T>::size_type host_vector<T>::size() const {
+inline typename host_vector<T>::size_type
+host_vector<T>::size() const {
   return m_vec.size();
 }
 
 template <typename T>
-inline T* host_vector<T>::data() {
+inline typename host_vector<T>::pointer
+host_vector<T>::data() {
   return m_vec.data();
 }
 
 
 template <typename T>
-inline const T * host_vector<T>::data() const {
+inline typename host_vector<T>::const_pointer
+host_vector<T>::data() const {
   return m_vec.data();
 }
 
